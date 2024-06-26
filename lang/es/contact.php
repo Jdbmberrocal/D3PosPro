a3f5d74cfd04591f9ffb2832a94e97960b8dbd1f
Jesus David Berrocal Mora <jdbmberrocal@gmail.com>
2024-06-26 16:31:18 -0500

    fsdfdf

---
+++ lang/es/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+ return [
+     'contacts' => 'Contactos',
+     'name' => 'Nombre',
+     'contact' => 'Contacto',
+     'manage_your_contact' => 'Administra tus :contacts',
+     'all_your_contact' => 'Todos sus :contacts',
+     'add_contact' => 'Agregar un nuevo contacto',
+     'contact_type' => 'Tipo de Contacto',
+     'tax_no' => 'Impuesto',
+     'pay_term' => 'Término de pago',
+     'pay_term_period' => 'Plazo de pago',
+     'mobile' => 'Celular',
+     'landline' => 'Telefono',
+     'alternate_contact_number' => 'Telefono alternativo',
+     'edit_contact' => 'Editar contacto',
+     'added_success' => 'Contacto agregado correctamente',
+     'updated_success' => 'Contacto actualizado con éxito',
+     'deleted_success' => 'Contacto eliminado correctamente',
+     'add_new_customer' => 'Agregar nuevo cliente',
+     'view_contact' => 'Ver contacto',
+     'contact_info' => 'Datos de :contact',
+     'all_purchases_linked_to_this_contact' => 'Todas las compras relacionadas con este contacto',
+     'all_sells_linked_to_this_contact' => 'Todas las ventas relacionadas con este contacto',
+     'total_purchase_due' => 'SALDO', /* MODIFICADO */
+     'pay_due_amount' => 'Cantidad adeudada',
+     'total_paid' => 'Total pagado',
+     'total_purchase_paid' => 'Pago total de compra',
+     'total_sale_paid' => 'Pago de venta total',
+     'total_sale_due' => 'Venta total debida', /* MODIFICADO */
+     'customer' => 'Cliente',
+     
+ ];
---
+++ lang/es/home.php
@@ -0,0 +1,22 @@
+<?php
+
+ return [
+     'home' => 'Inicio', /* modified */
+     'welcome_message' => 'Bienvenido :name',
+     'total_sell' => 'Ventas',
+     'total_purchase' => 'Compras totales', /* modified */
+     'invoice_due' => 'Factura a pagar',
+     'purchase_due' => 'Compra adeudada',
+     'today' => 'Hoy',
+     'this_week' => 'Esta semana',
+     'this_month' => 'Este mes',
+     'this_fy' => 'Este año', /* modified */
+     'sells_last_30_days' => 'Ventas últimos 30 días',
+     'sells_current_fy' => 'Año actual de ventas', /* modified */
+     'total_sells' => 'Ventas (:currency)',
+     'product_stock_alert' => 'Alerta de stock',
+     'payment_dues' => 'Cuota de pago',
+     'due_amount' => 'Cantidad debida',
+     'stock_expiry_alert' => 'Alerta de caducidad',
+     'todays_profit' => 'Beneficio de hoy',
+ ];
